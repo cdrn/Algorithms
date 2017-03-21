edaa770ee62f4c89c08913fbf055520d89ddd0ed
Theseu5 <chris.doran@connect.qut.edu.au>
2017-03-21 19:56:00 +1000

    Updated algorithm, basic structure

--- bruteForceMedian/main.cpp
+++ bruteForceMedian/main.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+
+    //Declare variables//
+    int numsmaller;
+    int numequal;
+    vector<int> A;
+    int numberRead;
+
+
+    //Open a file with filestream//
+    ofstream inputData;
+    inputData.open("inputData.csv");
+    while (inputData >> numberRead)
+    {
+        A.push_back(numberRead);
+    }
+    inputData.close();
+    //
+
+
+
     cout << "Hello world!" << endl;
-    return 0;
+
+
+
+    int K = A.size()/2;
+    int SizeOfArray = A.size();
+
+
+    for(int i = 0; i < (SizeOfArray-1); i++)
+    {
+        numsmaller = 0;
+        numequal = 0;
+        for(int j = 0; j < (SizeOfArray-1); j++)
+        {
+            if(A[j] < A[i])
+            {
+                numsmaller = numsmaller + 1;
+            }
+            else
+            {
+                if (A[j] < A[i])
+                {
+                    numequal = numequal + 1;
+                }
+            }
+        }
+        if(numsmaller<K and K <=(numsmaller + numequal))
+        {
+            return A[i];
+        }
+
+
+
+    }
+
 }
