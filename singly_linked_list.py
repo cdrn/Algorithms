6b5013e05a9eff31f3a8ed03c5fbca66b58a9497
Chris Doran <chrisdoran@protonmail.com>
2018-05-26 16:48:38 +1000

    scaffold delete linked list method

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -10,35 +10,63 @@ class LinkedList:
 
   # Represents a node in the linked list
   class Node:
-    def __init__(self, val, next=None):
+    def __init__(self, val=None, next=None):
       self.val = val
-      self.next = next  # Nothing indicated end node
+      self.next = next  # Nothing indicated means end node
     
     def get_next(self):
       return self.next
     
-    def get_val():
+    def get_val(self):
       return self.val
 
 
-  def __init__(val):
-    self.first_node = new Node(val=val)
+  def __init__(self, val):
+    self.first_node = self.Node(val)
     
-  def __traverse(linked_list):
-    node = linked_list.
+  def __traverse(self):
+    node = self.get_first_node()
     # Traverse the entire linked list to get to the end node
     while node.next:
+      print(node.get_val())
       node = node.next
     return node # return when invariant is false (please don't loop a linked list lmao)
       
-  def get_first_node():
+  def get_first_node(self):
     # Return the first node in the linked list
     return self.first_node
 
-  def add(value):
+  def add(self, val):
     # Pre: The value to add to the linked list
     # post: The value has been placed at the tail of the list
+    if not self.get_first_node():  # If no node exists, make one
+      self.first_node = Node(val)
+    else:  # else traverse to the final node and add
+      node = self.__traverse()
+      node.next = Node(value)
 
+  def contains(self, val):
+    # Assess whether the linked list contains a given value
+    if not self.get_first_node():
+      return False
+    node = self.get_first_node()
+    while node.get_val() is not val:
+      node = node.next
+    if node.get_val() == val:
+      return True
+    return False
+
+  def delete(self, val):
+    # Don't allow the first node to be deleted
+    if not self.get_first_node():
+      return False
+
+    node = self.get_first_node()
+    while val is not node.get_val():
+      node = node.next
+    # Handle the case of the node to be deleted being the first node
+    if node.get_val() == val:
+      if node == self.__traverse()
 
 
 
