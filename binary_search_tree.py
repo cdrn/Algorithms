5a4d7621466ae86bbeafb137f77652c736f2d5d3
Chris Doran <chrisdoran@protonmail.com>
2018-06-06 22:28:33 +1000

    bst contains, find node, find parent

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -22,19 +22,65 @@ class BinarySearchTree:
   def insert(self, root, value):
     node = self.Node(value)
     if root == None:
-      self.root = node
+      root = node
     else:
       if node.value > root.value:
         if root.right == None:
           root.right = node
         else:
           # Keep calling on the children until we find the correct node (traversing right node)
-          insert(root.right, node)
+          self.insert(root.right, value)
       # else if smaller or equal, push left node
       else:
         if root.left == None:
           root.left = node
         else:
-          insert(root.left, node)
+          self.insert(root.left, value)
+
+  # recursively search the bst to see if it contains a value
+  def contains(self, root, value):
+    if root is None:
+      print('root is none')
+      return False
+    print('contains: ', root.value, value)
+
+    if root.value == value:
+      print('returned true')
+      return True
+    elif value > root.value:
+      return self.contains(root.right, value)
+    else:
+      return self.contains(root.left, value)
+
+  # return a pointer to the parent node
+  def find_parent(self, root, value):
+    if value == root.value:
+      return root
+    if value > root.value:
+      if root.right == None:
+        return None
+      elif root.right.value == value:
+        return root
+      # recursively traverse the tree until we find our shit
+      else:
+        return self.find_parent(root.right, value)
+    else:
+      if root.left == None:
+        return None
+      elif root.left.value == value:
+        return root
+      else:
+        return self.find_parent(root.left, value)
+
+  # return a pointer to the actual node
+  def find_node(self, root, value):
+    if not root:
+      return None
+    if root.value == value:
+      return root
+    elif value > root.value:
+      self.find_node(root.right, value)
+    else:
+      self.find_node(root.left, value)
 
 
--- binary_search_tree_tests.py
+++ binary_search_tree_tests.py
@@ -3,8 +3,13 @@ import binary_search_tree
 
 # setup
 def init_bst():
-  return binary_search_tree.BinarySearchTree(25)
-  print(bst.root.value)
+  bst = binary_search_tree.BinarySearchTree(25)
+  bst.insert(bst.root, 10)
+  bst.insert(bst.root, 15)
+  bst.insert(bst.root, 500)
+  bst.insert(bst.root, 1000)
+  bst.insert(bst.root, 4)
+  return bst
 
 def test_bst_init():
   bst = init_bst()
@@ -16,3 +21,15 @@ def test_add_node_bst():
   print(bst.root.left)
   assert bst.root.left.value == 10
 
+def test_bst_contains():
+  bst = init_bst()
+  assert bst.contains(bst.root, 500)
+  
+def test_bst_contains_false():
+  bst = init_bst()
+  print(bst.contains(bst.root, 12512))
+  assert bst.contains(bst.root, 12512) == False
+
+def test_bst_get_parent():
+  bst = init_bst()
+  assert bst.find_parent(bst.root, 1000).value == 500
